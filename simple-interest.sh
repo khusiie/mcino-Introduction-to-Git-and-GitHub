b79105c2afea76c1c8225978ead1a9a6e3000a55
Khushi <kg7460502@gmail.com>
2025-12-24 15:09:24 +0530

    made chagnes in simple

--- simple-interest.sh
+++ simple-interest.sh
@@ -16,11 +16,11 @@
 
 echo "Enter the principal Amount :"
 read p
-echo "Enter rate of interest per year:"
+echo "Enter rate of interest per year :"
 read r
-echo "Enter time period in years:"
+echo "Enter time period in years :"
 read t
 
 s=$(expr $p \* $t \* $r / 100)
-echo "The simple interest is: "
+echo "The simple interest of this is: "
 echo $s
